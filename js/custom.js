d1102c6e11a73a717a48ce8a5aef81e52a999994
c mimes <c mimes>
2017-05-09 20:45:28 -0400

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,48 @@
+(function($) { 
+
+
+/* Back to Top*/
+$(document).ready(function(){
+	
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+	
+});
+
+
+
+
+
+/* HOVER MENU */
+$(document).ready(function(){
+    $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideDown("fast");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideUp("fast");
+            $(this).toggleClass('open');       
+        }
+    );
+});
+/* END HOVER MENU */
+
+})(jQuery);
+
+
+
+
+
